03d91eae5996578be158c013081e2b902f130c74
dicook <visnut@gmail.com>
2025-07-27 17:45:39 +1000

    materials for week 1

--- docs/week1/slides.R
+++ docs/week1/slides.R
@@ -1,24 +1,24 @@
-## ----include = FALSE-------------------------------
+## ----include = FALSE-----------------------------------
 source("../setup.R")
 
 
-## --------------------------------------------------
+## ------------------------------------------------------
 library(tidyverse)
 tips <- read_csv("http://ggobi.org/book/data/tips.csv")
 
 
-## ----echo = TRUE-----------------------------------
+## ----echo = TRUE---------------------------------------
 tips <- tips %>%
   mutate(tip_pct = tip/totbill * 100) 
 
 
-## ----echo = TRUE-----------------------------------
+## ----echo = TRUE---------------------------------------
 tips_lm <- tips %>%
   select(tip_pct, sex, smoker, day, time, size) %>%
   lm(tip_pct ~ ., data=.) 
 
 
-## ----modela, echo=TRUE, results="hide"-------------
+## ----modela, echo=TRUE, results="hide"-----------------
 library(broom)
 library(kableExtra)
 tidy(tips_lm) %>% 
@@ -26,20 +26,20 @@ tidy(tips_lm) %>%
   kable_styling() 
 
 
-## ----modelb, echo=TRUE, results="hide"-------------
+## ----modelb, echo=TRUE, results="hide"-----------------
 glance(tips_lm) %>% 
   select(r.squared, statistic, 
          p.value) %>% 
   kable(digits=3)
 
 
-## ----ref.label="modela", echo=FALSE----------------
+## ----ref.label="modela", echo=FALSE--------------------
 
 
-## ----ref.label="modelb", echo=FALSE----------------
+## ----ref.label="modelb", echo=FALSE--------------------
 
 
-## ----model_smalla, echo=TRUE, results='hide'-------
+## ----model_smalla, echo=TRUE, results='hide'-----------
 tips_lm <- tips %>%
   select(tip_pct, size) %>% 
   lm(tip_pct ~ ., data=.) 
@@ -48,19 +48,19 @@ tidy(tips_lm) %>%
   kable_styling() 
 
 
-## ----model_smallb, echo=TRUE, results='hide'-------
+## ----model_smallb, echo=TRUE, results='hide'-----------
 glance(tips_lm) %>% 
   select(r.squared, statistic, p.value) %>% 
   kable(digits=3)
 
 
-## ----ref.label="model_smalla", echo=FALSE----------
+## ----ref.label="model_smalla", echo=FALSE--------------
 
 
-## ----ref.label="model_smallb", echo=FALSE----------
+## ----ref.label="model_smallb", echo=FALSE--------------
 
 
-## ----res_hist, echo=TRUE, fig.show='hide'----------
+## ----res_hist, echo=TRUE, fig.show='hide'--------------
 tips_aug <- augment(tips_lm)
 ggplot(tips_aug, 
     aes(x=.resid)) + 
@@ -68,10 +68,10 @@ ggplot(tips_aug,
   xlab("residuals") 
 
 
-## ----res_hist2, ref.label="res_hist", echo=FALSE----
+## ----res_hist2, ref.label="res_hist", echo=FALSE-------
 
 
-## ----res_qq, echo=TRUE, fig.show='hide'------------
+## ----res_qq, echo=TRUE, fig.show='hide'----------------
 ggplot(tips_aug, 
     aes(sample=.resid)) + 
   stat_qq() +
@@ -80,10 +80,10 @@ ggplot(tips_aug,
   theme(aspect.ratio=1)
 
 
-## ----res_qq2, ref.label="res_qq", echo=FALSE-------
+## ----res_qq2, ref.label="res_qq", echo=FALSE-----------
 
 
-## ----obs_fitted, echo=TRUE, fig.show='hide'--------
+## ----obs_fitted, echo=TRUE, fig.show='hide'------------
 ggplot(tips_aug, 
     aes(x=.fitted, y=tip_pct)) + 
   geom_point() +
@@ -95,7 +95,7 @@ ggplot(tips_aug,
 ## ----obs_fitted2, ref.label="obs_fitted", echo=FALSE----
 
 
-## ----fitted_model, echo=TRUE, fig.show='hide'------
+## ----fitted_model, echo=TRUE, fig.show='hide'----------
 ggplot(tips_aug, 
     aes(x=size, y=tip_pct)) + 
   geom_point() +
@@ -106,30 +106,30 @@ ggplot(tips_aug,
 ## ----fitted_model2, ref.label="fitted_model", echo=FALSE----
 
 
-## --------------------------------------------------
+## ------------------------------------------------------
 #| echo: false
 options(width = 50)
 
 
-## --------------------------------------------------
+## ------------------------------------------------------
 glimpse(tips)
 
 
-## ----tips_hist, echo=TRUE, fig.show='hide'---------
+## ----tips_hist, echo=TRUE, fig.show='hide'-------------
 ggplot(tips, 
     aes(x=tip)) + 
   geom_histogram(
     colour="white")  
 
 
-## ----tips_hist2, ref.label="tips_hist", echo=FALSE----
+## ----tips_hist2, ref.label="tips_hist", echo=FALSE-----
 
 
-## ----tips_hist_fat, echo=TRUE, fig.show='hide'-----
+## ----tips_hist_fat, echo=TRUE, fig.show='hide'---------
 ggplot(tips, 
     aes(x=tip)) +
   geom_histogram(
-    breaks=seq(0.5,10.5,1), #<< 
+    breaks=seq(0.5,10.5,1),  
     colour="white") + 
   scale_x_continuous(
     breaks=seq(0,11,1))
@@ -138,11 +138,11 @@ ggplot(tips,
 ## ----tips_hist_fat2, ref.label="tips_hist_fat", echo=FALSE----
 
 
-## ----tips_hist_skinny, echo=TRUE, fig.show='hide'----
+## ----tips_hist_skinny, echo=TRUE, fig.show='hide'------
 ggplot(tips, 
     aes(x=tip)) + 
   geom_histogram(
-    breaks=seq(0.5,10.5,0.1), #<<
+    breaks=seq(0.5,10.5,0.1), 
     colour="white") +
   scale_x_continuous(
     breaks=seq(0,11,1))
@@ -151,31 +151,31 @@ ggplot(tips,
 ## ----tips_hist_skinny2, ref.label="tips_hist_skinny", echo=FALSE----
 
 
-## ----tips_tot, echo=TRUE, fig.show='hide'----------
+## ----tips_tot, echo=TRUE, fig.show='hide'--------------
 p <- ggplot(tips, 
     aes(x= totbill, y=tip)) + 
-  geom_point() + #<<
+  geom_point() + 
   scale_y_continuous(
     breaks=seq(0,11,1))
 p
 
 
-## ----tips_tot_b, ref.label="tips_tot", echo=FALSE----
+## ----tips_tot_b, ref.label="tips_tot", echo=FALSE------
 
 
-## ----tips_tot2, echo=TRUE, fig.show='hide'---------
-p <- p + geom_abline(intercept=0, #<<
-              slope=0.2) + #<<
+## ----tips_tot2, echo=TRUE, fig.show='hide'-------------
+p <- p + geom_abline(intercept=0, 
+              slope=0.2) + 
   annotate("text", x=45, y=10, 
            label="20% tip") 
 p
 
 
-## ----tips_tot2b, ref.label="tips_tot2", echo=FALSE----
+## ----tips_tot2b, ref.label="tips_tot2", echo=FALSE-----
 
 
-## ----tips_sexsmoke, echo=TRUE, fig.show='hide'-----
-p + facet_grid(smoker~sex) #<<
+## ----tips_sexsmoke, echo=TRUE, fig.show='hide'---------
+p + facet_grid(smoker~sex) 
 
 
 ## ----tips_sexsmokeb, ref.label="tips_sexsmoke", echo=FALSE, fig.width=9, fig.height=6, out.width="100%"----
